1806fe288bcb58c394bf41718c9f6386a95603e3
Gabriel <jgabriel.orozco@udea.edu.co>
2023-03-04 11:02:07 -0500

    Ahora se procesan las 3 regiones y se enmascara el color de piel a negro.

--- pdi_game.py
+++ pdi_game.py
@@ -23,75 +23,58 @@ x3 = int(w*3)
 y1 = int(h*0)
 y2 = int(h*1.5)
 
+# Mascara para el color de piel
+lower_hsv = np.array([18, 0, 0])
+higher_hsv = np.array([179, 255, 255])
+
 while True:
     # CAPTURA FOTOGRAMA 1
     
-    ret, frame1 = cap.read()
+    ret, frame = cap.read()
     # Refleja el fotograma
-    frame1 = cv2.flip(frame1, 1)
+    frame = cv2.flip(frame, 1)
 
     # Si no se puede capturar un fotograma, salir del bucle
     if not ret:
         break
 
-    # Convierte la imagen a escala de grises
-    frame1_gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    
     # Dibuja los rectángulos con borde negro de 2 píxeles de ancho
-    cv2.rectangle(frame1_gray, (x1, y2), (x1 + w, y2 + h), 0, thickness=2) # Izquierda
-    # cv2.rectangle(frame1_gray, (x1, y1), (x1 + w, y1 + h), 0, thickness=2) # Arriba-Izquierda
-    cv2.rectangle(frame1_gray, (x2, y1), (x2 + w, y1 + h), 0, thickness=2) # Arriba
-    # cv2.rectangle(frame1_gray, (x3, y1), (x3 + w, y1 + h), 0, thickness=2) # Arriba-Derecha
-    cv2.rectangle(frame1_gray, (x3, y2), (x3 + w, y2 + h), 0, thickness=2) # Derecha
+    cv2.rectangle(frame, (x1, y2), (x1 + w, y2 + h), 0, thickness=2) # Izquierda
+    # cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), 0, thickness=2) # Arriba-Izquierda
+    cv2.rectangle(frame, (x2, y1), (x2 + w, y1 + h), 0, thickness=2) # Arriba
+    # cv2.rectangle(frame, (x3, y1), (x3 + w, y1 + h), 0, thickness=2) # Arriba-Derecha
+    cv2.rectangle(frame, (x3, y2), (x3 + w, y2 + h), 0, thickness=2) # Derecha
 
     # Muestra el fotograma capturado
-    cv2.imshow('Capturando video', frame1_gray)
+    cv2.imshow('Capturando video', frame)
     
-    izquierda_frame1 = frame1_gray[y2:y2+h,x1:x1+w]
-    # cv2.imshow('Capturando izquierda', izquierda_frame1)
-    arriba_frame1 = frame1_gray[y1:y1+h,x2:x2+w]
-    # cv2.imshow('Capturando arriba', arriba_frame1)
-    derecha_frame1 = frame1_gray[y2:y2+h,x3:x3+w]
-    # cv2.imshow('Capturando derecha', derecha_frame1)
+    izquierda_frame = frame[y2:y2+h,x1:x1+w]
+    #cv2.imshow('Capturando izquierda', izquierda_frame)
+    arriba_frame = frame[y1:y1+h,x2:x2+w]
+    #cv2.imshow('Capturando arriba', arriba_frame)
+    derecha_frame = frame[y2:y2+h,x3:x3+w]
+    #cv2.imshow('Capturando derecha', derecha_frame)
 
-    # Espera 100 milisegundos y espera a que el usuario presione la tecla 'q' para salir
-    cv2.waitKey(100)
-    
-    # CAPTURA FOTOGRAMA 2
-    
-    ret, frame2 = cap.read()
-    # Refleja el fotograma
-    frame2 = cv2.flip(frame2, 1)
+    #Pasando a HSV
+    izquierda_hsv = cv2.cvtColor(izquierda_frame, cv2.COLOR_BGR2HSV)
+    arriba_hsv = cv2.cvtColor(arriba_frame, cv2.COLOR_BGR2HSV)
+    derecha_hsv = cv2.cvtColor(derecha_frame, cv2.COLOR_BGR2HSV)
 
-    # Si no se puede capturar un fotograma, salir del bucle
-    if not ret:
-        break
+    #Aplicando máscara de piel
+    izquierda_mask = cv2.inRange(izquierda_hsv, lower_hsv, higher_hsv)
+    arriba_mask = cv2.inRange(arriba_hsv, lower_hsv, higher_hsv)
+    derecha_mask = cv2.inRange(derecha_hsv, lower_hsv, higher_hsv)
 
-    # Convierte la imagen a escala de grises
-    frame2_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    
-    # Dibuja los rectángulos con borde negro de 2 píxeles de ancho
-    cv2.rectangle(frame2_gray, (x1, y2), (x1 + w, y2 + h), 0, thickness=2) # Izquierda
-    # cv2.rectangle(frame2_gray, (x1, y1), (x1 + w, y1 + h), 0, thickness=2) # Arriba-Izquierda
-    cv2.rectangle(frame2_gray, (x2, y1), (x2 + w, y1 + h), 0, thickness=2) # Arriba
-    # cv2.rectangle(frame2_gray, (x3, y1), (x3 + w, y1 + h), 0, thickness=2) # Arriba-Derecha
-    cv2.rectangle(frame2_gray, (x3, y2), (x3 + w, y2 + h), 0, thickness=2) # Derecha
-    
-    izquierda_frame2 = frame2_gray[y2:y2+h,x1:x1+w]
-    # cv2.imshow('Capturando izquierda', izquierda_frame2)
-    arriba_frame2 = frame2_gray[y1:y1+h,x2:x2+w]
-    # cv2.imshow('Capturando arriba', arriba_frame2)
-    derecha_frame2 = frame2_gray[y2:y2+h,x3:x3+w]
-    # cv2.imshow('Capturando derecha', derecha_frame2)
+    #Mostrar fragmentos procesados
+    cv2.imshow('Capturando izquierda', izquierda_mask)
+    cv2.imshow('Capturando arriba', arriba_mask)
+    cv2.imshow('Capturando derecha', derecha_mask)
 
     # Espera 100 milisegundos y espera a que el usuario presione la tecla 'q' para salir
     if cv2.waitKey(100) == ord('q'):
         break
-    
-    # LOGICA CON LOS FRAMES 1 Y 2
-    
-    
 
+    
 # Libera la cámara y cierra la ventana
 cap.release()
 cv2.destroyAllWindows()
