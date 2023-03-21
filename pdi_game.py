a3a358cac73c701463f89777e9e8275c66089dfe
Santa <santiago.santa@udea.edu.co>
2023-03-21 18:36:28 -0500

    Comentarios en formato solicitado.

--- pdi_game.py
+++ pdi_game.py
@@ -1,102 +1,142 @@
-#Importar librerías
-import numpy as np
-import cv2
-import keyboard
+#--------------------------------------------------------------------------
+#------- Juego de PDI------------------------------------------------------
+#------- Por: Santiago Santa Agudelo    santiago.santa@udea.edu.co --------
+#-------      Juan Gabriel Orozco Orozco    jgabriel.orozco@udea.edu.co ---
+#-------      Estudiantes de Ingeniería Electrónica -----------------------
+#------- Curso Básico de Procesamiento de Imágenes ------------------------
+#------- Marzo de 2023-----------------------------------------------------
+#--------------------------------------------------------------------------
 
-# keyboard.press('a')
-# keyboard.release('a')
+#--------------------------------------------------------------------------
+#------- Importación de Librerías -----------------------------------------
+#--------------------------------------------------------------------------
+
+import numpy as np  # Librería para el manejo de vectores y matrices multidimensionales.
+import cv2          # Librería para el manejo de imagenes
+import keyboard     # Librería para la ingestación de teclas
+
+#--------------------------------------------------------------------------
+#------- Seleccionar la cámara predeterminada -----------------------------
+#--------------------------------------------------------------------------
+
+
+cap = cv2.VideoCapture(0) # Selecciona la cámara predeterminada
+
+#--------------------------------------------------------------------------
+#------- Definición de las coordenadas para los rectángulos ---------------
+#--------------------------------------------------------------------------
 
 # Figuras de 480x640 pixeles
-# Selecciona la cámara predeterminada
-cap = cv2.VideoCapture(0)
-
-# Altura y ancho de frame
 height = 480
 width = 640
 
 # Coordenadas de rectángulos
-w = int(width/4)
-h = int(height/4)
-x1 = int(w*0)
-x2 = int(w*1.5)
-x3 = int(w*3)
-y1 = int(h*0)
-y2 = int(h*1.5)
+w = int(width/4)    # Anchura de cada rectángulo
+h = int(height/4)   # Altura de cada rectángulo
+x1 = int(w*0)       # Coordenada en X del rectángulo izquierdo
+x2 = int(w*1.5)     # Coordenada en X del rectángulo de arriba
+x3 = int(w*3)       # Coordenada en X del rectángulo derecho
+y1 = int(h*0)       # Coordenada en Y del rectángulo de arriba
+y2 = int(h*1.5)     # Coordenada en Y de los rectángulos izquierdo y derecho
+
+#--------------------------------------------------------------------------
+#------- Valores HSV para filtrar el color de la piel ---------------------
+#--------------------------------------------------------------------------
 
 # Mascara para el color de piel
-lower_hsv = np.array([0, 29, 0])
-higher_hsv = np.array([52, 186, 255])
+lower_hsv = np.array([0, 29, 0])        # El valor bajo de H, S y V es 0, 29 y 0 respectivamente
+higher_hsv = np.array([52, 186, 255])   # El valor alto de H, S y V es 0, 29 y 0 respectivamente
+
+#--------------------------------------------------------------------------
+#------- Ciclo donde se procesa un frame cada 100ms -----------------------
+#--------------------------------------------------------------------------
 
 while True:
-    # CAPTURA FOTOGRAMA 1
-    
-    ret, frame = cap.read()
-    # Refleja el fotograma
-    frame = cv2.flip(frame, 1)
+
+    #--------------------------------------------------------------------------
+    #------- Captura y reflejo del frame --------------------------------------
+    #--------------------------------------------------------------------------
+
+    ret, frame = cap.read()     # Captura del Frame
+    frame = cv2.flip(frame, 1)  # Se refleja el frame
 
     # Si no se puede capturar un fotograma, salir del bucle
     if not ret:
         break
 
+    #--------------------------------------------------------------------------
+    #------- Dibujar los rectángulos izquierdo, arriba y derecho --------------
+    #--------------------------------------------------------------------------
+
     # Dibuja los rectángulos con borde negro de 2 píxeles de ancho
     cv2.rectangle(frame, (x1, y2), (x1 + w, y2 + h), 0, thickness=2) # Izquierda
-    # cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), 0, thickness=2) # Arriba-Izquierda
     cv2.rectangle(frame, (x2, y1), (x2 + w, y1 + h), 0, thickness=2) # Arriba
-    # cv2.rectangle(frame, (x3, y1), (x3 + w, y1 + h), 0, thickness=2) # Arriba-Derecha
     cv2.rectangle(frame, (x3, y2), (x3 + w, y2 + h), 0, thickness=2) # Derecha
 
     # Muestra el fotograma capturado
     cv2.imshow('Capturando video', frame)
+
+    #--------------------------------------------------------------------------
+    #------- Capturar los rectángulos izquierdo, arriba y derecho -------------
+    #--------------------------------------------------------------------------
     
-    izquierda_frame = frame[y2:y2+h,x1:x1+w]
-    #cv2.imshow('Capturando izquierda', izquierda_frame)
-    arriba_frame = frame[y1:y1+h,x2:x2+w]
-    #cv2.imshow('Capturando arriba', arriba_frame)
-    derecha_frame = frame[y2:y2+h,x3:x3+w]
-    #cv2.imshow('Capturando derecha', derecha_frame)
+    izquierda_frame = frame[y2:y2+h,x1:x1+w]    # Izquierda
+    arriba_frame = frame[y1:y1+h,x2:x2+w]       # Arriba
+    derecha_frame = frame[y2:y2+h,x3:x3+w]      # Derecha
 
-    #Pasando a HSV
-    izquierda_hsv = cv2.cvtColor(izquierda_frame, cv2.COLOR_BGR2HSV)
-    arriba_hsv = cv2.cvtColor(arriba_frame, cv2.COLOR_BGR2HSV)
-    derecha_hsv = cv2.cvtColor(derecha_frame, cv2.COLOR_BGR2HSV)
+    #--------------------------------------------------------------------------
+    #------- Llevar los rectángulos al formato HSV y filtar el color piel -----
+    #--------------------------------------------------------------------------
 
-    #Aplicando máscara de piel
-    izquierda_mask = cv2.inRange(izquierda_hsv, lower_hsv, higher_hsv)
-    arriba_mask = cv2.inRange(arriba_hsv, lower_hsv, higher_hsv)
-    derecha_mask = cv2.inRange(derecha_hsv, lower_hsv, higher_hsv)
+    # Pasando a HSV
+    izquierda_hsv = cv2.cvtColor(izquierda_frame, cv2.COLOR_BGR2HSV)    # Izquierda
+    arriba_hsv = cv2.cvtColor(arriba_frame, cv2.COLOR_BGR2HSV)          # Arriba
+    derecha_hsv = cv2.cvtColor(derecha_frame, cv2.COLOR_BGR2HSV)        # Derecha
+
+    # Filtrando el color piel -> se obtiene pixel blanco si detecta el color piel
+    # y pixel negro si hay ausencia de color piel
+    izquierda_mask = cv2.inRange(izquierda_hsv, lower_hsv, higher_hsv)  # Izquierda
+    arriba_mask = cv2.inRange(arriba_hsv, lower_hsv, higher_hsv)        # Arriba
+    derecha_mask = cv2.inRange(derecha_hsv, lower_hsv, higher_hsv)      # Derecha
 
     #Mostrar fragmentos procesados - Debug Only
     #cv2.imshow('Capturando izquierda', izquierda_mask)
     #cv2.imshow('Capturando arriba', arriba_mask)
     #cv2.imshow('Capturando derecha', derecha_mask)
 
-    #Promedios para detección de mano
+    #--------------------------------------------------------------------------
+    #------- Detección de color piel en los rectángulos -----------------------
+    #--------------------------------------------------------------------------
+
+    # Promedios para detección del color piel (promedio de los pixeles blancos)
     izquierda_mean = int(np.mean(izquierda_mask))
     arriba_mean = int(np.mean(arriba_mask))
     derecha_mean = int(np.mean(derecha_mask))
     
     #Umbrales de detección
-    if izquierda_mean > 50:
-        print("Izquierda")
+    if izquierda_mean > 50:     # Si en el recángulo de la izquierda el promedio de pixeles blancos es mayor
+        print("Izquierda")      # a 50, se presiona la tecla 'a'
         keyboard.press('a')
     else:
-        keyboard.release('a')
-    if arriba_mean > 50:
-        print("Arriba")
+        keyboard.release('a')   # De lo contrario libere la tecla 'a'
+    if arriba_mean > 50:        # Si en el recángulo de arriba el promedio de pixeles blancos es mayor
+        print("Arriba")         # a 50, se presiona la tecla 'w'
         keyboard.press('w')
     else:
-        keyboard.release('w')
-    if derecha_mean > 50:
-        print("Derecha")
+        keyboard.release('w')   # De lo contrario libere la tecla 'w'
+    if derecha_mean > 50:       # Si en el recángulo de la derecha el promedio de pixeles blancos es mayor
+        print("Derecha")        # a 50, se presiona la tecla 'd'
         keyboard.press('d')
     else:
-        keyboard.release('d')
-    
+        keyboard.release('d')   # De lo contrario libere la tecla 'd'
 
-    # Espera 100 milisegundos y espera a que el usuario presione la tecla 'q' para salir
+    #--------------------------------------------------------------------------
+    #------- Condición finalización del ciclo y espera de 100ms ---------------
+    #--------------------------------------------------------------------------
+
+    # Espera 100 milisegundos y si el usuario presiona la tecla 'q' se cierra el programa
     if cv2.waitKey(100) == ord('q'):
         break
-
     
 # Libera la cámara y cierra la ventana
 cap.release()
